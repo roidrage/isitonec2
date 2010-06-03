aa032803b4686aacb200fd05d375262fbb57973d
Mathias Meyer <meyer@paperplanes.de>
2010-06-03 14:30:32 +0200

    Just trying something

--- app.rb
+++ app.rb
@@ -2,5 +2,6 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  params[:host] + ": " + `( host -t A #{params[:host]} | awk "{print $4}" | xargs whois | grep ec2 > /dev/null && echo "Runs on EC2") || echo "Not on EC2"`
+  #params[:host] + ": " + `( host -t A #{params[:host]} | awk "{print $4}" | xargs whois | grep ec2 > /dev/null && echo "Runs on EC2") || echo "Not on EC2"`
+  params[:host] + ": " + `( host -t A scalarium.com | awk "{print $4}" | xargs whois | grep ec2 > /dev/null && echo "Runs on EC2") || echo "Not on EC2"`
 end
