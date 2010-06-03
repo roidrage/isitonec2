4ab75312d49ab0388825fdc766b0df6cd1b0d3bf
Mathias Meyer <meyer@paperplanes.de>
2010-06-03 13:18:14 +0200

    More dynamic

--- app.rb
+++ app.rb
@@ -2,5 +2,5 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  `whois scalarium.com`
+  params[:host] + ": " + `( host -t A #{params[:host]} | awk "{print $4}" | xargs whois | grep ec2 > /dev/null && echo "Runs on EC2") || echo "Not on EC2"`
 end
