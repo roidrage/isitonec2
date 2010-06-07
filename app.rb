1b0f57ff4fe60c9af6ec4ee0104007a2067d809c
Mathias Meyer <meyer@paperplanes.de>
2010-06-07 20:56:28 +0200

    Added shortcut to call app directly with a host name.

--- app.rb
+++ app.rb
@@ -3,12 +3,20 @@ require 'sinatra'
 require 'whois'
 require 'resolv'
 
+def is_it_on_ec2
+  @isonec2 = (Whois.whois(Resolv.new.getaddress(params[:host])).content.grep(/ec2/).any? ? "on EC2" : "not on EC2")
+end
+
 get '/' do
   erb :index
 end
 
 post '/' do
-  @isonec2 = (Whois.whois(Resolv.new.getaddress(params[:host])).content.grep(/ec2/).any? ? "on EC2" : "not on EC2")
+  is_it_on_ec2
   erb :show
 end
 
+get '/:host' do
+  is_it_on_ec2
+  erb :show
+end
