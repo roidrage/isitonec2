2694476a25a180e45b960b9bf4f9587633f91536
Mathias Meyer <meyer@paperplanes.de>
2010-06-07 18:58:43 +0200

    Added some basic form, needs styling.

--- app.rb
+++ app.rb
@@ -3,6 +3,34 @@ require 'sinatra'
 require 'whois'
 require 'resolv'
 
+enable :inline_templates
 get '/' do
-  params[:host] + ": " + (Whois.whois(Resolv.new.getaddress(params[:host])).content.grep(/ec2/).any? ? "Runs on EC2" : "Not on EC2")
+  erb :index
 end
+
+post '/' do
+  @isonec2 = (Whois.whois(Resolv.new.getaddress(params[:host])).content.grep(/ec2/).any? ? "on EC2" : "not on EC2")
+  erb :show
+end
+
+
+__END__
+
+@@layout
+
+<html>
+  <head><title>Is it on EC2?</title></head>
+  <body>
+    <%= yield %>
+  </body>
+</html>
+
+@@index
+
+<form action="/" method="post">
+  Hostname: <input name="host" type="text"/>
+</form>
+
+@@show
+
+Host <%= params[:host] %> is <%= @isonec2 %>.
