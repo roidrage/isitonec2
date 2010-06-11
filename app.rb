d141b99284df56395bf1f5c45857fb67fab8b3fc
Mathias Meyer <meyer@paperplanes.de>
2010-06-11 11:29:55 +0200

    Parse input as URI, extracting the host.
    Falls back to just the host if URI parsing failed, e.g. when only a host name was specified.

--- app.rb
+++ app.rb
@@ -5,6 +5,8 @@ require 'resolv'
 
 def is_it_on_ec2
   begin
+    uri = URI.parse(params[:host])
+    params[:host] = uri.host || params[:host]
     @isonec2 = (Whois.whois(Resolv.new.getaddress(params[:host])).content.grep(/ec2/).any? ? "is on EC2" : "is not on EC2")
   rescue
     @isonec2 = "probably doesn't exist"
